a9e04956ddf41481310265af66022efbd8d9eed2
longpv <long.phamvan@adamosoft.com>
2025-09-11 13:10:47 +0700

    style 2

--- add-shortcode.php
+++ add-shortcode.php
@@ -698,13 +698,26 @@ function lv_card_category_shortcode($atts)
                 echo '<a target="' . $link_target . '" href="' . $link_url . '" class="lv_block_card_category_card" style="background-color:' . $background_color . ';">';
 
                 // Display image if available
+                if ($style_category == '2') {
+                    echo '<div class="lv_block_card_category_inner">';
+                }
                 if ($image_html) {
                     echo '<div class="lv_block_card_category_icon">' . $image_html . '</div>';
                 }
 
                 // Display title and description
+                if ($style_category == '2') {
+                    echo '<div class="lv_block_card_category_content">';
+                }
                 echo '<div class="lv_block_card_category_title">' . $link_title . '</div>';
                 echo '<div class="lv_block_card_category_desc">' . $description . '</div>';
+                if ($style_category == '2') {
+                    echo '</div></div>';
+
+                    echo '<div class="lv_block_card_category_right">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.0.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M566.6 342.6C579.1 330.1 579.1 309.8 566.6 297.3L406.6 137.3C394.1 124.8 373.8 124.8 361.3 137.3C348.8 149.8 348.8 170.1 361.3 182.6L466.7 288L96 288C78.3 288 64 302.3 64 320C64 337.7 78.3 352 96 352L466.7 352L361.3 457.4C348.8 469.9 348.8 490.2 361.3 502.7C373.8 515.2 394.1 515.2 406.6 502.7L566.6 342.7z"/></svg>
+                    </div>';
+                }
 
                 echo '</a>'; // End of card
             }
